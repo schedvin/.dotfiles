d8611f5c2a8185d4b4ca5734373d965005b5579c
Carl Johan Schedvin <cj.schedvin@gmail.com>
2023-01-07 17:19:13 +0100

    Add ftplugins, nvim-tree plugin

--- roles/neovim/files/after/plugin/defaults.lua
+++ roles/neovim/files/after/plugin/defaults.lua
@@ -1,4 +1,3 @@
-
 vim.o.swapfile = false
 vim.o.backup = false
 
@@ -10,9 +9,6 @@ vim.o.expandtab = true
 -- Make relative line numbers default
 vim.wo.relativenumber = true
 vim.o.smartindent = true
-
-
-
 
 -- Save undo history
 vim.o.undofile = true
@@ -33,50 +29,53 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = "Move visual selection dow
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- delete to unnamed register
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- cancel
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- LSP keymaps
+vim.keymap.set("v", "<leader>ff", "<cmd>lua vim.lsp.buf.format()<cr>")
+vim.keymap.set("v", "<leader>fv", "<cmd>lua vim.lsp.buf.format()<cr>")
 
-
-vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[Search] [K]eymaps'})
+-- Telescope keymaps
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[Search] [K]eymaps' })
 
 
 vim.keymap.set('n', '<leader>zz', ':ZenMode<cr>')
 
 vim.keymap.set("n", "<leader>st", "<cmd>TodoTelescope<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 vim.keymap.set("n", "<leader>tt", "<cmd>TodoTrouble<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 vim.keymap.set("n", "<leader>tl", "<cmd>TodoLocList<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 
--- folke/trouble.nvim 
+-- folke/trouble.nvim
 vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-  {silent = true, noremap = true}
+  { silent = true, noremap = true }
 )
 
 
--- roles/neovim/files/lua/custom/plugins.lua
+++ roles/neovim/files/lua/custom/plugins.lua
@@ -1,4 +1,38 @@
 return function(use)
+
+  -- TODO: find another way to set these before netrw is loaded, should
+  -- placed a top of `init.lua` (strongly advised)
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+  vim.opt.termguicolors = true
+
+  use {
+  "nvim-tree/nvim-tree.lua",
+    requires = {
+      "nvim-tree/nvim-web-devicons", -- optional, for file icons
+    },
+    tag = "nightly", -- optional, updated every week. (see issue #1193)
+    config = function ()
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = {
+          adaptive_size = true,
+          mappings = {
+            list = {
+              { key = "u", action = "dir_up" },
+            },
+          },
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = true,
+        },
+      })
+    end
+  }
+
   use({
     "folke/zen-mode.nvim",
       config = function()
@@ -6,32 +40,35 @@ return function(use)
       end
   })
 
+  -- higlight and show todo comments like TODO, HACK, BUG, NOTE
+  -- See `:help todo-comments.txt`
   use {
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
     config = function()
       require("todo-comments").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        -- custom configuration goes here
       }
     end
   }
+
+  -- colorscheme tokyonight
   use {
     "folke/tokyonight.nvim"
   }
+
+  -- prettify diagnostics, LSP, telescope results, qf ... 
   use {
     "folke/trouble.nvim",
     requires = "kyazdani42/nvim-web-devicons",
     config = function()
       require("trouble").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+         -- custom configuration goes here
       }
     end
   }
 
+  -- applies shading to inactive windows
   use {
     "sunjon/shade.nvim",
     config = function ()
